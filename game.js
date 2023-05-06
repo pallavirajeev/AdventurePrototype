4e5d69dbdc4d7dbbda186a4b69d5f3a36344a6f2
Pallavi Rajeev <pallavirajeev44@gmail.com>
2023-05-05 19:10:45 -0700

    add spell book

--- game.js
+++ game.js
@@ -5,58 +5,58 @@ class Demo1 extends AdventureScene {
 
     onEnter() {
 
-        let clip = this.add.text(this.w * 0.3, this.w * 0.3, "📎 paperclip")
+        let book = this.add.text(this.w * 0.3, this.w * 0.3, "📖 spell book")
             .setFontSize(this.s * 2)
             .setInteractive()
-            .on('pointerover', () => this.showMessage("Metal, bent."))
+            .on('pointerover', () => this.showMessage("Oooh, a spell book!."))
             .on('pointerdown', () => {
-                this.showMessage("No touching!");
+                this.showMessage("You pick up the spell book.");
+                this.gainItem('spell book');
                 this.tweens.add({
-                    targets: clip,
-                    x: '+=' + this.s,
-                    repeat: 2,
-                    yoyo: true,
-                    ease: 'Sine.inOut',
-                    duration: 100
-                });
-            });
-
-        let key = this.add.text(this.w * 0.5, this.w * 0.1, "🔑 key")
-            .setFontSize(this.s * 2)
-            .setInteractive()
-            .on('pointerover', () => {
-                this.showMessage("It's a nice key.")
-            })
-            .on('pointerdown', () => {
-                this.showMessage("You pick up the key.");
-                this.gainItem('key');
-                this.tweens.add({
-                    targets: key,
+                    targets: book,
                     y: `-=${2 * this.s}`,
                     alpha: { from: 1, to: 0 },
                     duration: 500,
-                    onComplete: () => key.destroy()
+                    onComplete: () => book.destroy()
                 });
-            })
+            });
 
-        let door = this.add.text(this.w * 0.1, this.w * 0.15, "🚪 locked door")
-            .setFontSize(this.s * 2)
-            .setInteractive()
-            .on('pointerover', () => {
-                if (this.hasItem("key")) {
-                    this.showMessage("You've got the key for this door.");
-                } else {
-                    this.showMessage("It's locked. Can you find a key?");
-                }
-            })
-            .on('pointerdown', () => {
-                if (this.hasItem("key")) {
-                    this.loseItem("key");
-                    this.showMessage("*squeak*");
-                    door.setText("🚪 unlocked door");
-                    this.gotoScene('demo2');
-                }
-            })
+        // let key = this.add.text(this.w * 0.5, this.w * 0.1, "🔑 key")
+        //     .setFontSize(this.s * 2)
+        //     .setInteractive()
+        //     .on('pointerover', () => {
+        //         this.showMessage("It's a nice key.")
+        //     })
+        //     .on('pointerdown', () => {
+        //         this.showMessage("You pick up the key.");
+        //         this.gainItem('key');
+        //         this.tweens.add({
+        //             targets: key,
+        //             y: `-=${2 * this.s}`,
+        //             alpha: { from: 1, to: 0 },
+        //             duration: 500,
+        //             onComplete: () => key.destroy()
+        //         });
+        //     })
+
+        // let door = this.add.text(this.w * 0.1, this.w * 0.15, "🚪 locked door")
+        //     .setFontSize(this.s * 2)
+        //     .setInteractive()
+        //     .on('pointerover', () => {
+        //         if (this.hasItem("key")) {
+        //             this.showMessage("You've got the key for this door.");
+        //         } else {
+        //             this.showMessage("It's locked. Can you find a key?");
+        //         }
+        //     })
+        //     .on('pointerdown', () => {
+        //         if (this.hasItem("key")) {
+        //             this.loseItem("key");
+        //             this.showMessage("*squeak*");
+        //             door.setText("🚪 unlocked door");
+        //             this.gotoScene('demo2');
+        //         }
+        //     })
 
     }
 }
